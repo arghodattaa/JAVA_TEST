a3b8c7d755cfd20429c34e7b79d4e7507c398ac8
arghodattaa <aurghodotto@gmail.com>
2025-06-08 08:01:37 +0600

    Update Start.java

--- lab/LAB1/start/Start.java
+++ lab/LAB1/start/Start.java
@@ -6,7 +6,7 @@ import operation1.StudentOperation1;
 public class Start {
     public static void main(String []agsh){
         ArrayList< StudentOperation1> students = new ArrayList<>();
-        students.add(new StudentOperation1("Argho Kumar Datta ", "24-57530-2", 3.83, 6));
+        students.add(new StudentOperation1("Argho Kumar Datta ", "24-57530-2", 0.0, 6));
 
         for( StudentOperation1 obj : students){
             obj.allShow();
